6f7600c697cb06a3952180d5f189f210267e3b7f
Rogerio Neto <rogerio.neto@castgroup.com.br>
2026-02-21 14:10:28 -0300

    Atualizando a nova instancia criada

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://136.248.120.4:8090/api',
+    apiUrl: 'http://136.248.118.51:8090/api',
     featureFlag: {
         enableNewFeature: true,
     },
