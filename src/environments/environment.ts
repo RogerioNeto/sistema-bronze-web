db7e18882fd6a1d7c8387e5f28cdb4550d07b318
Rogerio Neto <rogerio.neto@castgroup.com.br>
2026-02-21 16:24:57 -0300

    corrigindo autenticador

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: true,
     apiUrl: 'http://136.248.118.51:8090/api',
+    apiUrl0: 'http://136.248.118.51:8090',
     featureFlag: {
         enableNewFeature: true,
     },
