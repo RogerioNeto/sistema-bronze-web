c762a81775d02cf7d52a2a7d9c97f1c3be66507d
Rogerio Neto <rogerio.neto@castgroup.com.br>
2026-03-04 16:57:43 -0300

    all

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://app-ba676d71-8e00-445e-8d8b-312fe138d180.cleverapps.io:8080/api',
-    //apiUrl: 'http://localhost:8080/api',
+    apiUrl: 'https://app-ba676d71-8e00-445e-8d8b-312fe138d180.cleverapps.io',
     featureFlag: {
         enableNewFeature: true,
     },
